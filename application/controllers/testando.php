97b8a68909dc5c22222f6d79aaf0fcc645ecfc2b
Pratera <matheuspwk@hotmail.com>
2022-02-16 00:17:02 -0300

    Inicio do Projeto

---
+++ application/controllers/testando.php
@@ -0,0 +1,10 @@
+<?php
+   class testando extends CI_Controller {
+
+      function index(){
+      /*carrega a nossa view */
+         $this->load->view('testando');
+         
+      }
+   }
+?>
---
+++ application/views/testando.php
@@ -0,0 +1,15 @@
+<html>
+    
+    <head>
+        
+          <title>Olá Mundo!</title>
+    
+    </head>
+    
+    <body>
+        
+        <h1> Bem Vindo! </h1>
+    
+    </body>
+
+</html>
